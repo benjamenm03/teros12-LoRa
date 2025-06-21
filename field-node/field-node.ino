71bfe98178d327ef0a7115df5b2031c53d3b7375
benjamenm03 <benjamen@umich.edu>
2025-06-21 15:47:15 -0400

    adding larger offset for nodes

--- field-node/field-node.ino
+++ field-node/field-node.ino
@@ -180,7 +180,7 @@ void loop() {
 #endif
       firstSlot = false;
     } else {
-      uint16_t offset = 15 + 15 * NODE_ID;        // 30 s for node-1
+      uint16_t offset = 15 + 30 * NODE_ID;        // 30 s for node-1
       announceSleep(F("node offset"), offset, true);
     }
 
