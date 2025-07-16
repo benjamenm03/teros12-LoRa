cea7164fcb8677c12a25634de61bbff1986ab137
benjamenm03 <benjamen@umich.edu>
2025-07-16 14:52:24 -0400

    new main, with CAD in base station

--- base-station/base-station.ino
+++ base-station/base-station.ino
@@ -243,6 +243,7 @@ void sendEpochTo(uint8_t dest)
   uint32_t now32 = nowEpoch32();
   snprintf(msg, sizeof(msg), "TIME:%" PRIu32, now32);
 
+  rf95.waitCAD();
   rf95.send(reinterpret_cast<uint8_t*>(msg), strlen(msg));
   rf95.waitPacketSent();
 
@@ -351,9 +352,10 @@ void loop()
           if (sep == -1) break; else start = sep + 1;
         }
 
-        delay(5000);
+        delay(1000);
         char ack[28];
         snprintf(ack, sizeof(ack), "ACKTIME:%" PRIu32, nowEpoch32());
+        rf95.waitCAD();
         rf95.send(reinterpret_cast<uint8_t*>(ack), strlen(ack));
         rf95.waitPacketSent();
         Serial.printf("→ %s\n", ack);
